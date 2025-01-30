4d31feca05816139884835d9f8b0b9887a4db9a0
Naveen Nannapaneni <19226381+vnann@users.noreply.github.com>
2025-01-30 11:50:29 -0500

    updated the docker-run and requiremetns

--- mydev/docker-run.sh
+++ mydev/docker-run.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 IMAGE_VERSION="v1"
 IMAGE_NAME="mydev"
 CONTAINER_NAME="$IMAGE_NAME-$IMAGE_VERSION-$USER"
 
-echo "Starting python dev environemnt container....."
+echo "Starting dev environemnt container....."
 echo "------------------------------------------------------"
 echo " Docker Image Name:        $IMAGE_NAME"
 echo " Docker Container Name:    $CONTAINER_NAME"
@@ -12,5 +12,5 @@ echo " Docker image:version:     $IMAGE_NAME:$IMAGE_VERSION"
 echo "------------------------------------------------------"
 
 docker run -it --name $CONTAINER_NAME --rm \
- --volume $(pwd):/home/devuser/$IMAGE_NAME \
+ --volume $(pwd):/root/$IMAGE_NAME \
  --net=host  $IMAGE_NAME:$IMAGE_VERSION
